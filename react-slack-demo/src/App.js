0e54cceb5d19aa4b287ef15ec5a9984cd33ac550
Manish Jha <manishjhazyx@gmail.com>
2021-12-09 22:17:05 +0530

    initial commit

---
+++ react-slack-demo/src/App.js
@@ -0,0 +1,15 @@
+
+import {useState, useEffect} from 'react';
+import {StreamChat} from 'stream-chat';
+const API_KEY = process.env.REACT_APP_API_KEY
+
+function App() {
+    const [chatClient, setChatClient] = useState(null);
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
